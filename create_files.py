6eeb2625ba93458dd6d96c2aa4db5eb021a93bfb
Kraig Britton <kbritton@tripwire.com>
2018-01-04 23:28:42 -0800

    Added a little exception handling

--- create_files.py
+++ create_files.py
@@ -7,14 +7,18 @@ def createFiles():
     a template for the class as it's written with a string iteratively replaced
     in each file.
     """
+    
     for i in range(100):
         filename = str(i).zfill(3) + ".pp"
         str_in_file = 'myfile' + str(i).zfill(3)
-
-        with open("sample_class.txt", "rt") as fin:
-            with open(filename, "wt") as fout:
-                for line in fin:
-                    fout.write(line.replace('myfile000', str_in_file))
+        try:
+            with open("sample_class.txt", "rt") as fin:
+                with open(filename, "wt") as fout:
+                    for line in fin:
+                        fout.write(line.replace('myfile000', str_in_file))
+        except IOError:
+            print("The file, as specified, does not exist...")
+            break
 
 
 createFiles()
